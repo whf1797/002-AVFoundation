930b009db00976d855ed256ec34151190d086f48
王洪飞 <wanghongfei@wanghongfeideMacBook-Pro.local>
2018-11-21 22:52:23 +0800

    init

--- 003-AVFoundation/ViewController.h
+++ 003-AVFoundation/ViewController.h
@@ -7,9 +7,60 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+extern NSString *const THThumbnailCreateNotification;
+
+@protocol THcameraControllerDelegate <NSObject>
+
+-(void)deviceConfigurationFailedWithError:(NSError *)error;
+-(void)mediaCaptureFailedWithError:(NSError *)error;
+-(void)assetLibraryWriteFailedWithError:(NSError *)error;
+
+
+@end
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) id <THcameraControllerDelegate>delegate;
+@property (nonatomic, strong , readonly)AVCaptureSession *captureSession;
 
+-(BOOL)setupSession:(NSError *)error;
+-(void)startSession;
+-(void)stopSession;
+
+-(BOOL)switchCameras;
+-(BOOL)canSwitchCameras;
+
+@property (nonatomic, readonly)NSUInteger cameraCount;
+
+@property (nonatomic, readonly)BOOL cameraHasTorch; // 手电筒
+
+@property (nonatomic, readonly)BOOL camearHasFlash; //闪光灯
+
+@property (nonatomic, readonly)BOOL cameraSupportsTapToFocus; // 聚焦
+
+@property (nonatomic, readonly)BOOL cameraSupportsTapToExpose;// 曝光
+
+@property (nonatomic)AVCaptureTorchMode torchMode;
+
+@property (nonatomic)AVCaptureFlashMode flashMode;
+
+// 聚焦 曝光 重设聚焦 曝光的方法
+-(void)focusAtPoint:(CGPoint)point;
+-(void)exposeAtPoint:(CGPoint)point;
+-(void)resetFocusAndExposureModes;
+
+// 实现捕捉静态图片 视频的g功能
+-(void)captureStillImage;
+
+// 开始录制
+-(void)startRecording;
+// 停止录制
+-(void)stopRecording;
+// 获取录制状态
+-(BOOL)isRecording;
+// 录制时间
+-(CMTime)recordedDuration;
 @end
 
--- 003-AVFoundation/ViewController.m
+++ 003-AVFoundation/ViewController.m
@@ -8,7 +8,17 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+
+NSString *const THThumbnailCreateNotification = @"THThumbnailCreated" ;
+
+@interface ViewController ()<AVCaptureFileOutputRecordingDelegate>
+
+@property (strong, nonatomic)dispatch_queue_t videoQueue; //视频队列
+@property (strong, nonatomic)AVCaptureSession *captureSession;// 捕捉会话
+@property (weak, nonatomic)AVCaptureDeviceInput *activeVideoInput; //输入
+@property (strong, nonatomic)AVCaptureMovieFileOutput *moveOutput;
+@property (strong, nonatomic)NSURL *outputURL;
+
 
 @end
 
@@ -18,6 +28,82 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+-(BOOL)setupSession:(NSError *)error
+{
+    self.captureSession = [[AVCaptureSession alloc] init];
+    // 设置分辨率
+    self.captureSession.sessionPreset = AVCaptureSessionPreset640x480;
+    
+    // 拿到默认视频捕捉设备 ios系统返回后置摄像头
+    AVCaptureDevice *videoDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    //
+    AVCaptureDeviceInput *videoinput = [AVCaptureDeviceInput deviceInputWithDevice:videoDevice error:&error];
+    
+    if (videoinput) {
+        // canAddinput 测试能否添加到会话中
+        if ([self.captureSession canAddInput:videoinput]) {
+            [self.captureSession addInput:videoinput];
+            self.activeVideoInput = videoinput;
+            
+        }
+    }else{
+        return NO;
+    }
+    
+    // 选择默认音频捕捉设备 即返回一个内置麦克风
+    AVCaptureDevice *audioDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeAudio];
+    // 为设备创建一个捕捉设备输入
+    AVCaptureDeviceInput *audioInput = [AVCaptureDeviceInput deviceInputWithDevice:audioDevice error:&error];
+    
+    if (audioInput) {
+        if ([self.captureSession canAddInput:audioInput]) {
+            [self.captureSession addInput:audioInput];
+        }
+    }else{
+        return NO;
+    }
+
+    
+    self.videoQueue = dispatch_queue_create("cc.videoqueue", DISPATCH_QUEUE_SERIAL);
+    
+    
+    return YES;
+}
+
+
+-(void)startSession
+{
+    if (![self.captureSession isRunning]) {
+        dispatch_async(self.videoQueue, ^{
+            [self.captureSession startRunning];
+        });
+    }
+}
+
+
+-(void)stopSession
+{
+    if ([self.captureSession isRunning]) {
+        dispatch_async(self.videoQueue, ^{
+            [self.captureSession stopRunning];
+        });
+    }
+}
+
+
+-(AVCaptureDevice *)activeCamera
+{
+    return self.activeVideoInput.device;
+}
+
+-(AVCaptureDevice *)inactiveCamera
+{
+    return nil;
+}
+
+
 
 
 @end
